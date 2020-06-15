4083d032f5422b50bf8837c351b34830676669b7
Hemanth R <hemanthrt18@gmail.com>
2020-06-15 14:54:35 +0530

    nav-toggler

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -25,9 +25,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = False
 
-ALLOWED_HOSTS = ['csablog.herokuapp.com']
+
+ALLOWED_HOSTS = ['csablog.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
